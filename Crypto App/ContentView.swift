a182d7771b2137f81cb7cd6cf6da17cf0119085c
Dipak Prasad <dipak@thecodebucket.com>
2022-07-25 14:31:10 +0530

    Initial Commit

---
+++ Crypto App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crypto App
+//
+//  Created by Xcode on 25/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crypto App/Crypto_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_AppApp.swift
+//  Crypto App
+//
+//  Created by Xcode on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
