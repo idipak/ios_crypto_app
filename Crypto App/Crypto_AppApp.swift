95bcb788b59e31bcb78054f13df5d2ef85ebe416
Dipak Prasad <dipak@thecodebucket.com>
2022-08-10 19:45:01 +0530

    Coin Search

--- Crypto App/Crypto_AppApp.swift
+++ Crypto App/Crypto_AppApp.swift
@@ -12,6 +12,11 @@ struct Crypto_AppApp: App {
     
     @StateObject private var vm = HomeViewModel()
     
+    init(){
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView{
